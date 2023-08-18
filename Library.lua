3dd9bf50910eb8e09b34d00d286d3fff61a24627
VampireOnDiscord <105789900+VampireOnDiscord@users.noreply.github.com>
2023-08-18 23:03:52 +0400

    Library.lua

--- Library.lua
+++ Library.lua
@@ -9,8 +9,6 @@ local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
-local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
-
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
